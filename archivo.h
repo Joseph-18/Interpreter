f19e36f66c329990b7f4b01ff23d004499220580
Joseph Perez <joseph.pg@almisasolutions.com>
2016-11-10 00:54:28 -0400

    Documentando la clase lexico y elarchivo.h

--- archivo.h
+++ archivo.h
@@ -1,16 +1,21 @@
 # include <stdlib.h>
 # include <fstream>
 # include "lexico.cpp"
-
 using namespace std;
 
+/** Archivo de cabecera para leer un archivo .txt.
+* Se carga el archivo lexico.cpp para verificar el lexico de la expresion.
+*/
+
 void get_file() {
+	/** Metodo que lee un archivo .txt y extrae los datos linea por linea verificando que este
+	* lexicamente correcto.
+	*/
 	char linea[500];
 	ifstream fe("Expresiones.txt");
 	while(fe.peek() != EOF) {
 		fe.getline(linea, 500);
 		Lexico obj1;
-		//obj1.lex(string(linea));
 		printf("%d\n", obj1.lex(string(linea)));
 	};
 };
--- lexico.cpp
+++ lexico.cpp
@@ -2,6 +2,7 @@
 # include <string>
 using namespace std;
 
+// Arreglo de todos los caracteres posibles que pueden haber en una expresion.
 char elementos[77] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
 					'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
 					'0','1','2','3','4','5','6','7','8','9','_','(',')',
@@ -9,9 +10,16 @@ char elementos[77] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o
 
 
 class Lexico {
+/** Clase en la cual se encuentran los metodos necesarios para comprobar el lexico de una expresion.
+* Se encuentran 3 metodos que verifican si los elementos que se encuentran en la expresino son validos.
+*/
 
 public:
 	bool lex(string linea) {
+	/** Metodo que toma una varible de tipo cadena y verifica que todos sus caracteres sean validos para una expresion.
+	* @param linea de tipo String 
+	* @return un booleano que significa si los caracteres en la expresion son validos.
+	*/
 
 		bool logic = true;
 		for (int i = 0; i < linea.length(); ++i) {
@@ -25,6 +33,10 @@ public:
 		return logic;
 	};
 	bool inputElement(string linea, int i) {
+		/** Metodo que vefica los caracteres especiales que contienen 2 caracteres.
+		* @param linea de tipo string e un indice.
+		* @return un booleano verificando elementos multivaluados.
+		*/
 		//printf("%d\n", buscarElement(char(linea[i])));
 		if (buscarElement(char(linea[i]))== 1) {
 			if (linea[i] == '!') {
@@ -56,6 +68,10 @@ public:
 		return true;
 	};
 	int buscarElement(char letter) {
+		/** Metodo que verifica si existe un caracter en los elementos validos.
+		* @param letter de tipo char, letra que se va a buscar.
+		* @return un integer, diciendo si es valido o no.
+		*/
 
 		for (int i = 0; i < 77; ++i) {
 			if (letter == elementos[i]){
